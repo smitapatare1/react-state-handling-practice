96fd1ce66167780dce1684c61e5b1d3a681b315a
smitapatare1 <smitapatare2002@gmail.com>
2022-12-05 21:23:05 +0530

    changes added in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from 'react';
 function App() {
+const [count, setCount]=useState(0);
+
+function increment()
+{
+  setCount(count+1);
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>useState</h1>
+      <h2>count={count}</h2>
+      <h2>second count={count}</h2>
+      <button onClick={increment}>Increase Count</button>
+    </>
   );
 }
 
